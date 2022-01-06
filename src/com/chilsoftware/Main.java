22991972a235da259cbb642e604b5b209ca8be88
chilkwon <chilhoan.kwon@candu.org>
2022-01-05 23:10:22 -0500

    first Git commit with Michael's code

---
+++ src/com/chilsoftware/Main.java
@@ -0,0 +1,11 @@
+package com.chilsoftware;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Schedule sc = new Schedule();
+        sc.AddingSchedule();
+         System.out.println(sc.getList().contains("chil"));
+    }
+}
